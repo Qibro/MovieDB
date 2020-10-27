8fa985f164f30474f935bb1dd97a6e4b3555c523
Qibro <muhamad.ibrahim21@gmail.com>
2020-10-28 02:50:35 +0700

    remove unused resource and import

--- app/src/main/java/org/mibrahim/movie/MyApplication.kt
+++ app/src/main/java/org/mibrahim/movie/MyApplication.kt
@@ -1,7 +1,6 @@
 package org.mibrahim.movie
 
 import android.app.Application
-import android.widget.Toast
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
